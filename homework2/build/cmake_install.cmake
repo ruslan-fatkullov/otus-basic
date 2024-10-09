6589da22804ec57bad0960ca251472136a7b9b7a
ruslan-fatkullov <fatkullov1999@gmail.com>
2024-10-10 00:09:38 +0400

    do homework2

--- homework2/build/cmake_install.cmake
+++ homework2/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/C++Projects/homework2
+# Install script for directory: C:/C++Projects/otus-basic/homework2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/C++Projects/homework2/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/C++Projects/otus-basic/homework2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
